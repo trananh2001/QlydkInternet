0defcbf1074ed86042efa0136427dced986f736c
Ha Thanh Doan <thanhdoanyura@gmail.com>
2018-04-28 22:46:04 +0700

    branch Khach_hang_dk

---
+++ QlydkInternet/Controllers/HopDongController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QlydkInternet.Controllers
+{
+    public class HopDongController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult TaoMoi()
+        {
+            return View();
+        }
+        public IActionResult DanhSach()
+        {
+            return View();
+        }
+        public IActionResult Xem()
+        {
+            return View();
+        }
+        public IActionResult ChinhSua()
+        {
+            return View();
+        }
+    }
+}
